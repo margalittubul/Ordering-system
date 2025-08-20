a875ae023106b6bcbf9756354998e8c280163923
margalit_tubul <margalittubul@gmail.com>
2025-08-20 14:55:17 +0300

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,22 @@ const port = process.env.PORT || 3000;
 
 connectDB(); 
 
-app.use(cors()); 
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://ordering-system-1-rxqk.onrender.com" 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+// app.use(cors()); 
 
 app.use(bodyParser.json()); 
 app.use(express.json()); 
